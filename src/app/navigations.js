7c41a906386dadf25cc45c23bc7163da16a835a6
hanspintor <hans.pintor365@gmail.com>
2022-04-18 13:44:55 +0800

    initial commit of project

---
+++ src/app/navigations.js
@@ -0,0 +1,27 @@
+export const navigations = [
+    {
+        name: 'Dashboard',
+        path: '/dashboard/default',
+        icon: 'dashboard',
+    },
+    {
+        name: 'Courier',
+        path: '/material/table',
+        icon: 'local_shipping',
+    },
+    {
+        name: 'Delivery Prices',
+        path: '/material/table',
+        icon: 'price_change',
+    },
+    {
+        name: 'Reports',
+        path: '/material/table',
+        icon: 'warning',
+    },
+    {
+        name: 'Cash-outs',
+        path: '/material/table',
+        icon: 'account_balance_wallet',
+    },
+]
